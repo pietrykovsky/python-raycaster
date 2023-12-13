e6b275b0d89cb9cd03b7d0270b67a5436532c107
Miłosz Bochenek <miloszbochenek13@gmail.com>
2023-12-14 00:41:54 +0100

    Fixing bugs with scaling when window resize happen v.2

--- raycaster/const.py
+++ raycaster/const.py
@@ -6,7 +6,6 @@ from raycaster.core import Settings
 DELTA_ANGLE = math.radians(Settings().FOV / Settings().RAY_COUNT)
 
 # MAP RELATED
-SCREEN_DISTANCE = (
-    (Settings().SCREEN_WIDTH // 2)
-    / math.tan(math.radians(Settings().FOV // 2))
+SCREEN_DISTANCE = (Settings().SCREEN_WIDTH // 2) / math.tan(
+    math.radians(Settings().FOV // 2)
 )
